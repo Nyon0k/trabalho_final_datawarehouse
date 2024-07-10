90b9c737213483e344893ee757b598db3ce74d5d
Daniel Silva <returndaniels@gmail.com>
2024-07-09 23:32:52 -0300

    doc: Add doc string on methods

--- etl/etl_2.py
+++ etl/etl_2.py
@@ -2,6 +2,17 @@ import pandas as pd
 import os
 
 def create_df_dtempo(df, df_dtempo_existing):
+    """
+    Create a dataframe for the dimension table 'df_dtempo' based on the given input dataframe and existing 'df_dtempo' dataframe.
+
+    Args:
+        df (pandas.DataFrame): Input dataframe containing the data.
+        df_dtempo_existing (pandas.DataFrame): Existing 'df_dtempo' dataframe.
+
+    Returns:
+        pandas.DataFrame: The updated 'df_dtempo' dataframe.
+
+    """
     df_aux = pd.DataFrame()
     df_aux['timestamp'] = pd.to_datetime(df['data'])
 
@@ -21,7 +32,7 @@ def create_df_dtempo(df, df_dtempo_existing):
     else:
         df_dtempo_new['tempo_key'] = df_dtempo_new.index + 1
         df_dtempo_export = df_dtempo_new
-        
+
     df_dtempo_export[['tempo_key', 'ano', 'mes', 'dia', 'hora']].to_csv(os.path.join(os.getcwd(), 'dados/df_dtempo.csv'), index=False)
     # df_dtempo_export.to_csv(os.path.join(os.getcwd(), 'dados/df_dtempo.csv'), index=False)
 
@@ -72,6 +83,20 @@ def create_df_destacao(df, df_destacao_existing):
     return df_destacao_export
 
 def create_df_fqualidadear(df, df_dtempo, df_dlocalizacao, df_destacao, df_fqualidadear_existing):
+    """
+    Create a new DataFrame for fqualidadear data by merging and processing the given input DataFrames.
+
+    Args:
+        df (pandas.DataFrame): The main DataFrame containing the data.
+        df_dtempo (pandas.DataFrame): The DataFrame containing time-related data.
+        df_dlocalizacao (pandas.DataFrame): The DataFrame containing location-related data.
+        df_destacao (pandas.DataFrame): The DataFrame containing station-related data.
+        df_fqualidadear_existing (pandas.DataFrame): The existing DataFrame for fqualidadear data.
+
+    Returns:
+        pandas.DataFrame: The resulting DataFrame for fqualidadear data.
+
+    """
     df_aux = df.copy()
     df_aux['timestamp'] = pd.to_datetime(df['data'])
 
@@ -90,13 +115,13 @@ def create_df_fqualidadear(df, df_dtempo, df_dlocalizacao, df_destacao, df_fqual
     else:
         df_fqualidadear_export = df_fqualidadear_new
 
-    # Remover duplicatas com base nas colunas especificadas
+    # Remove duplicates based on specified columns
     df_fqualidadear_export = df_fqualidadear_export.drop_duplicates(subset=['tempo_key', 'estacao_key', 'localizacao_key', 'chuva', 'pres', 'rs', 'temp', 'ur', 'dir_vento', 'vel_vento', 'so2', 'no2', 'hcnm', 'hct', 'ch4', 'co', 'no', 'nox', 'o3', 'pm10', 'pm2_5'])
 
-    # Remover colunas 'id' se existirem
+    # Remove 'id' columns if they exist
     df_fqualidadear_export = df_fqualidadear_export.loc[:, ~df_fqualidadear_export.columns.str.startswith('id')]
 
-    # Adicionar nova coluna 'id'
+    # Add a new 'id' column
     df_fqualidadear_export.reset_index(drop=True, inplace=True)
     df_fqualidadear_export.index.name = 'id'
     
@@ -105,6 +130,16 @@ def create_df_fqualidadear(df, df_dtempo, df_dlocalizacao, df_destacao, df_fqual
     return df_fqualidadear_export
 
 def etl_function(df):
+    """
+    Performs the ETL (Extract, Transform, Load) process on the given DataFrame.
+
+    Parameters:
+    - df (pandas.DataFrame): The input DataFrame containing the data to be processed.
+
+    This function drops unnecessary columns from the input DataFrame and creates or updates
+    several other DataFrames based on the extracted data. The resulting DataFrames are then
+    printed to the console.
+    """
     df = df.drop(columns=['x_utm_sirgas2000', 'y_utm_sirgas2000'])
 
     # Carregar os DataFrames existentes, se existirem
@@ -121,6 +156,17 @@ def etl_function(df):
     print(df_fqualidadear)
 
 def predata_validator(file_path, history_path, etl_function):
+    """
+    Validates the input data file and performs the ETL process if necessary.
+
+    Args:
+        file_path (str): The path to the input data file.
+        history_path (str): The path to the history file.
+        etl_function (function): The function to be used for the ETL process.
+
+    Raises:
+        FileNotFoundError: If the input data file does not exist.
+    """
     # Verifica se os arquivos existem
     if not os.path.exists(file_path):
         raise FileNotFoundError(f"O arquivo {file_path} não foi encontrado.")
